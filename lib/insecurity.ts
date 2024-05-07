bbf5af03408ffe87405ad73a3755674aa995e1de
Kamil <a@b.c>
2024-05-07 10:30:11 +0200

    lab3

--- lib/insecurity.ts
+++ lib/insecurity.ts
@@ -194,7 +194,10 @@ export const appendUserId = () => {
 export const updateAuthenticatedUsers = () => (req: Request, res: Response, next: NextFunction) => {
   const token = req.cookies.token || utils.jwtFrom(req)
   if (token) {
-    jwt.verify(token, publicKey, (err: Error | null, decoded: any) => {
+    const verifyOptions = {
+      algorithms: ['RS256'] // Specify allowed algorithms here
+    };
+    jwt.verify(token, publicKey, verifyOptions, (err: Error | null, decoded: any) => {
       if (err === null) {
         if (authenticatedUsers.get(token) === undefined) {
           authenticatedUsers.put(token, decoded)
