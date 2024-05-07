0ff6f84a22f79585aeac26597831f74891471cdd
Kamil <a@b.c>
2024-05-07 10:16:34 +0200

    lab4

--- routes/search.ts
+++ routes/search.ts
@@ -19,6 +19,10 @@ class ErrorWithParent extends Error {
 module.exports = function searchProducts () {
   return (req: Request, res: Response, next: NextFunction) => {
     let criteria: any = req.query.q === 'undefined' ? '' : req.query.q ?? ''
+    const validator = /^[a-zA-Z0-9\s]*$/
+    if(!validator.test(criteria)){
+      return res.status(400).json({error: 'Only letter and numbers allowed'})
+    }
     criteria = (criteria.length <= 200) ? criteria : criteria.substring(0, 200)
     models.sequelize.query(`SELECT * FROM Products WHERE ((name LIKE '%${criteria}%' OR description LIKE '%${criteria}%') AND deletedAt IS NULL) ORDER BY name`) // vuln-code-snippet vuln-line unionSqlInjectionChallenge dbSchemaChallenge
       .then(([products]: any) => {
